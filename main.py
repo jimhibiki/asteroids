8687325cc1537528e9b894a7e05d669004b29ff1
jimhibiki <jimhibiki@sbcglobal.net>
2025-03-07 20:49:31 -0600

    Asteroid First Load

---
+++ main.py
@@ -0,0 +1,30 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+# cd ~/workspace/github.com/jimhibiki/asteroids
+# source venv/bin/activate
+# MAN I STRUGGLED WITH THIS.  So... always activate the virtual environment: source venv/bin/activate
+# Run VcXsrv
+# Also, not sure if it was needed but added the following to ~/.bashrc
+# export DISPLAY=$(ip route list default | awk '{print $3}'):0
+# export LIBGL_ALWAYS_INDIRECT=1
+import pygame
+from constants import *
+
+def main():
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    linf=1
+    black=[0,0,0]
+    while linf==1:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen,black)
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
